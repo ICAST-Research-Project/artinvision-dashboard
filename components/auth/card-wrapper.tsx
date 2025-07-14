80c4241cc8eca0e3682d021ced847ba858d49284
Nischal Dinesh <nischaldgowda77@gmail.com>
2025-07-13 20:27:15 -0500

    fixed vercel build errors

--- components/auth/card-wrapper.tsx
+++ components/auth/card-wrapper.tsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 import { Card, CardContent, CardFooter, CardHeader } from "../ui/card";
 import { BackButton } from "./back-button";
 import { Header } from "./header";
-import { Social } from "./social";
 
 interface CardWrapperProps {
   children: React.ReactNode;
