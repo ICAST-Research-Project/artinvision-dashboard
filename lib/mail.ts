c9060b5a8849eec5a29eb91929bf9b5ca81b3f12
Nischal Dinesh <nischaldgowda77@gmail.com>
2025-10-16 14:40:54 -0500

    feat: updated offical domin name

--- lib/mail.ts
+++ lib/mail.ts
@@ -3,10 +3,10 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendVerificationEmail = async (email: string, token: string) => {
-  const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
+  const confirmLink = `https://www.art-connect.org/auth/new-verification?token=${token}`;
 
   await resend.emails.send({
-    from: "support@nischaldinesh.com",
+    from: "support@art-connect.org",
     to: email,
     subject: "Confirm your Email",
     html: `<p>Click <a href="${confirmLink}">here</a> to confirm email</p>`,
@@ -14,10 +14,10 @@ export const sendVerificationEmail = async (email: string, token: string) => {
 };
 
 export const sendPasswordEmail = async (email: string, token: string) => {
-  const resetLink = `http://localhost:3000/auth/new-password?token=${token}`;
+  const resetLink = `https://www.art-connect.org/auth/new-password?token=${token}`;
 
   await resend.emails.send({
-    from: "support@nischaldinesh.com",
+    from: "support@art-connect.org",
     to: email,
     subject: "Reset your password",
     html: `<p>Click <a href="${resetLink}">here</a> to reset password</p>`,
